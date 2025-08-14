a428b76526d67bb655a98fb5142ef4992e5bbc23
Lumpii <104854776+Lumpii404@users.noreply.github.com>
2025-08-14 13:34:45 +0200

    Add files via upload

---
+++ ADD ME/items.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('package1', 'Package', 800, 0, 1),
+('package2', 'Package', 800, 0, 1),
+('package3', 'Package', 800, 0, 1);
+('package4', 'Package', 800, 0, 1),
+('package5', 'Package', 800, 0, 1),
+('package6', 'Package', 800, 0, 1);
--- config.lua
+++ config.lua
@@ -61,20 +61,36 @@ Translation = {
         ['text'] = 'Du hast %s %s aus der %s bekommen.',
         ['text_2'] = 'Du hast keine ',
         ['point'] = '. ',
+        ['help_place_or_stow'] = 'Drücke ~INPUT_PICKUP~ um das Paket abzulegen oder ~INPUT_DETONATE~ um es wegzupacken.',
+        ['help_open_or_pickup'] = 'Drücke ~INPUT_PICKUP~ um das Paket zu öffnen oder ~INPUT_DETONATE~ um es aufzuheben.',
+        ['notify_stowed'] = 'Paket wurde weggeräumt.',
+        ['progress_loading'] = 'Paket wird geöffnet...'
     },
     ['en'] = {
         ['text'] = 'You got %s %s from the %s.',
         ['text_2'] = 'You have no ',
         ['point'] = '. ',
+        ['help_place_or_stow'] = 'Press ~INPUT_PICKUP~ to place the package or ~INPUT_DETONATE~ to stow it.',
+        ['help_open_or_pickup'] = 'Press ~INPUT_PICKUP~ to open the package or ~INPUT_DETONATE~ to pick it up.',
+        ['notify_stowed'] = 'Package stowed.',
+        ['progress_loading'] = 'Opening package...'
     },
     ['es'] = {
         ['text'] = 'Tienes %s %s del %s.',
         ['text_2'] = 'No tienes ',
         ['point'] = '. ',
+        ['help_place_or_stow'] = 'Presiona ~INPUT_PICKUP~ para colocar el paquete o ~INPUT_DETONATE~ para guardarlo.',
+        ['help_open_or_pickup'] = 'Presiona ~INPUT_PICKUP~ para abrir el paquete o ~INPUT_DETONATE~ para recogerlo.',
+        ['notify_stowed'] = 'Paquete guardado.',
+        ['progress_loading'] = 'Abriendo paquete...'
     },
     ['fr'] = {
         ['text'] = 'Tu as obtenu %s %s du %s.',
         ['text_2'] = "Tu n'as pas ",
         ['point'] = '. ',
+        ['help_place_or_stow'] = 'Appuyez sur ~INPUT_PICKUP~ pour poser le paquet ou ~INPUT_DETONATE~ pour le ranger.',
+        ['help_open_or_pickup'] = 'Appuyez sur ~INPUT_PICKUP~ pour ouvrir le paquet ou ~INPUT_DETONATE~ pour le ramasser.',
+        ['notify_stowed'] = 'Paquet rangé.',
+        ['progress_loading'] = 'Ouverture du paquet...'
     },
 }
